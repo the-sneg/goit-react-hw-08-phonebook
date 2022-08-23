5654eb78fa340e17976e12fb72bc0531f147ce2c
the-sneg <the.sneg.sasha@gmail.com>
2022-08-23 10:57:49 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import { Form } from './Form/Form';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
+import { nanoid } from 'nanoid';
 import s from './App.module.css';
 
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Вася Джонсон', number: '589-12-224' },
-      { id: 'id-2', name: 'Валерій Коперфілд', number: '323-89-124' },
-      { id: 'id-3', name: 'Джавеліна Бавовна', number: '544-17-791' },
-      { id: 'id-4', name: 'Олег Сміт', number: '227-41-222' },
-      { id: 'id-5', name: 'Вікторія Насамс', number: '257-91-263' },
+      { name: 'Вася Джонсон', number: '589-12-224', id: nanoid() },
+      { name: 'Валерій Коперфілд', number: '323-89-124', id: nanoid() },
+      { name: 'Джавеліна Бавовна', number: '544-17-791', id: nanoid() },
+      { name: 'Олег Сміт', number: '227-41-222', id: nanoid() },
+      { name: 'Вікторія Насамс', number: '257-91-263', id: nanoid() },
     ],
     filter: '',
   };
 
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+    if (contacts !== null) {
+      this.setState({ contacts: JSON.parse(contacts) });
     }
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
     }
   }
 
